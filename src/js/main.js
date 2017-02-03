1c2300daf8d7d36e32e121f0f1faa127bd004060
Pedro Lima <pedro.nym@gmail.com>
2017-02-03 23:10:29 +0000

    Microsite is now working. Should be good to go.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
+(function (win, doc) {
+  var overlay  = document.querySelector('.overlay'),
+      sound    = document.querySelector('audio'),
+      onButton = document.querySelector('.on-button');
 
+  function toggleOverlay (event) {
+    overlay.classList.add('hidden');
+    sound.play();
+  }
+
+  onButton.addEventListener('click', toggleOverlay);
+})(window, document);
